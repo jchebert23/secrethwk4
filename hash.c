fdb27e588cf6ed0ae30bfac0e6120b02a5f8fed9
Hebert Clayton <jch223@zoo.cs.yale.edu>
2018-11-04 23:00:16 -0500

    i guess hash is working with default prefix set to -1

--- hash.c
+++ hash.c
@@ -9,7 +9,7 @@
 #include <inttypes.h>
 #include <errno.h>
 #include <limits.h>
-int debugPrint1=0;
+int debugPrint1=1;
 typedef struct pair{
     int nchar;
     int pref;
@@ -22,10 +22,12 @@ typedef struct hash{
 
 int init(int p, int c, int max)
 {
+	p = abs(p);
 	return (p % max);
 }
 
 int stepfunc(int p, int c, int max){
+	p = abs(p);
 	return ((2 * (((p)<<CHAR_BIT)|(c)) + 1) % max);
 }
 
@@ -33,7 +35,7 @@ void addToHash(pair **table,int max, int p , int c){
     int start = init(p, c, max);
     int step = stepfunc(p,c, max);
     if(debugPrint1){printf("Adding nchar: %d, and pref: %d to with start: %d and step: %d\n", c, p, start, step);}
-    while(table[start] || start==0){start= (start + step) % max;}
+    while(table[start]){start= (start + step) % max;}
     table[start]=malloc(sizeof(pair));
     table[start]->nchar=c;
     table[start]->pref=p;
@@ -42,7 +44,7 @@ void addToHash(pair **table,int max, int p , int c){
 
 pair * search(pair **table, int max, int p, int c){
     int start = init(p, c, max);
-    while(table[start] || start==0){
+    while(table[start]){
     if(table[start]->pref == p && table[start]->nchar == c){ return table[start];}
     start= (start + stepfunc(p, c, max)) % max;}
     return 0;
@@ -57,7 +59,7 @@ hash * initHash(int rows)
     pair ** table= h->table;
     for(int i=0; i<256; i++)
     {
-	addToHash(table, h->curbits, 0, i);	
+	addToHash(table, h->curbits, -1, i);	
     }
     return h;
 }
